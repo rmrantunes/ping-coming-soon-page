c24af27ed789ec7561257124ddb1ddb679c51f95
Rafael Antunes <rafaelmessias710@gmail.com>
2020-09-27 08:18:19 -0400

    Input field validation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const renderAlert = (state = "error") => {
   const messages = {
     error: "<b>Please provide a valid email address</b>",
     success: "<b>Yay, we just send you an email!</b>",
+    empty: "<b>Whoops! It looks like you forgot to add your email</b>",
   };
 
   return `
@@ -24,16 +25,19 @@ const init = () => {
   formElement.setAttribute("novalidate", "");
 
   formElement.addEventListener("submit", (event) => {
-    if (!validationRegex.test(emailElement.value.trim())) {
+    if (emailElement.value == "") {
+      alertElement.innerHTML = renderAlert("empty");
+    } else if (!validationRegex.test(emailElement.value.trim())) {
       event.preventDefault();
       alertElement.innerHTML = renderAlert("error");
       emailElement.classList.add("error");
       emailElement.setAttribute("aria-invalid", "true");
       return;
+    } else {
+      formElement.removeChild(emailElement);
+      formElement.removeChild(buttonElement);
+      alertElement.innerHTML = renderAlert("success");
     }
-    formElement.removeChild(emailElement);
-    formElement.removeChild(buttonElement);
-    alertElement.innerHTML = renderAlert("success");
     event.preventDefault();
   });
 };
